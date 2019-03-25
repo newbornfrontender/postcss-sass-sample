e30f482df9e770555e3cee2f7936d8a861b187c2
Infant Frontender <newbornfrontender@gmail.com>
2019-03-25 15:11:13 +0300

    feat: set up postcss-sorting

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,23 @@ module.exports = ({ env, options: { map } }) => ({
 
   plugins: {
     'postcss-mixins': true,
+
     'precss': true,
+
     'cssnano': env === 'production',
+
+    'postcss-sorting': {
+      'order': [
+        'custom-properties',
+        'dollar-variables',
+        'declarations',
+        'at-rules',
+        'rules',
+      ],
+
+      'properties-order': 'alphabetical',
+
+      'unspecified-properties-position': 'bottom',
+    },
   },
 });
