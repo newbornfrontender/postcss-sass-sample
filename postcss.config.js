220b354ecf2749497775c30fa733b262d2a4b6b3
Infant Frontender <newbornfrontender@gmail.com>
2019-03-25 16:22:26 +0300

    style: format code

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,15 @@
 module.exports = ({ env, options: { map } }) => ({
-  parser: 'postcss-scss',
-
   map: env === 'production' && map,
 
   plugins: {
     'postcss-mixins': true,
 
-    'precss': true,
+    precss: true,
 
-    'cssnano': env === 'production',
+    cssnano: env === 'production',
 
     'postcss-sorting': {
-      'order': [
-        'custom-properties',
-        'dollar-variables',
-        'declarations',
-        'at-rules',
-        'rules',
-      ],
+      order: ['custom-properties', 'dollar-variables', 'declarations', 'at-rules', 'rules'],
 
       'properties-order': 'alphabetical',
 
